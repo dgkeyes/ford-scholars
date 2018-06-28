7d4fddb828e7352d52739380d068ed188eed11f9
dgkeyes <dgkeyes@gmail.com>
2018-06-28 10:11:19 -0700

    Reverse order of 3.2 responses

--- scratch.R
+++ scratch.R
@@ -1,13 +1,13 @@
 data %>%
-     select(how_interested_are_you_in_engaging_with_the_alumni_association) %>%
+     select(contains("reasons_for_engaging")) %>%
+     select(-reasons_for_engaging_with_alumni_assocation_is_there_anything_not_listed_in_question_13_that_would_motivate_you_to_participate_in_alumni_activities) %>%
+     set_names(c("Professional networking",
+                 "Community service",
+                 "Building community",
+                 "Leadership development",
+                 "Connection to and support of TFFF")) %>%
      gather(key = "item", value = "response") %>%
-     filter(!is.na(response)) %>%
-     mutate(response = ifelse(response == "Somewhat interested" |
-                                   response == "Very interested",
-                              "Y", "N")) %>%
-     group_by(item, response) %>%
-     summarize(n = n()) %>%
-     
-     mutate(pct = round(prop.table(n), 2)) %>%
-     filter(response == "Y") %>%
-     mutate(group = "All respondents") 
+     mutate(response = 6 - response) %>%
+     group_by(item) %>%
+     summarize(avg = round(mean(response, na.rm = TRUE), 1)) %>%
+     mutate(group = "All respondents")
